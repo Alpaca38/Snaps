8fa3faef750dafff4cf119953cf0714c4960dae1
Alpaca38 <137505484+Alpaca38@users.noreply.github.com>
2024-07-22 13:25:02 +0900

    Initial Commit

---
+++ Snaps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Snaps
+//
+//  Created by 조규연 on 7/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
