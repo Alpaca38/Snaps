53168e68a8af2c44c9e399324f0c6da6109d63e1
Alpaca38 <137505484+Alpaca38@users.noreply.github.com>
2024-08-09 22:19:58 +0900

    TabBarController 열거형으로 개선

--- Snaps/View/TabBarController.swift
+++ Snaps/View/TabBarController.swift
@@ -11,21 +11,82 @@ final class TabBarController: UITabBarController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tabBar.tintColor = .black
-        
-        let topic = UINavigationController(rootViewController: TopicPhotoViewController())
-        topic.tabBarItem = UITabBarItem(title: "토픽", image: Image.tabTrendInactive, selectedImage: Image.tabTrend)
-        
-        let random = UINavigationController(rootViewController: RandomPhotoViewController())
-        random.tabBarItem = UITabBarItem(title: "랜덤 사진", image: Image.tabRandomInactive, selectedImage: Image.tabRandom)
-        
-        let search = UINavigationController(rootViewController: SearchPhotoViewController())
-        search.tabBarItem = UITabBarItem(title: "검색", image: Image.tabSearchInActive, selectedImage: Image.tabSearch)
-        
-        let like = UINavigationController(rootViewController: LikesViewController())
-        like.tabBarItem = UITabBarItem(title: "찜", image: Image.tabLikeInactive, selectedImage: Image.tabLike)
-        
-        setViewControllers([topic, random, search, like], animated: true)
+        setTabBarController()
     }
     
+    func setTabBarController() {
+        tabBar.tintColor = .black
+        
+        var viewControllers: [UIViewController] = []
+        
+        TabBar.allCases.forEach {
+            let tab = UINavigationController(rootViewController: $0.viewController)
+            tab.tabBarItem = UITabBarItem(title: $0.title, image: $0.image, selectedImage: $0.selectedImage)
+            
+            viewControllers.append(tab)
+        }
+        
+        setViewControllers(viewControllers, animated: true)
+    }
+}
+
+extension TabBarController {
+    enum TabBar: CaseIterable {
+        case topic
+        case random
+        case search
+        case like
+        
+        var viewController: UIViewController {
+            switch self {
+            case .topic:
+                return TopicPhotoViewController()
+            case .random:
+                return RandomPhotoViewController()
+            case .search:
+                return SearchPhotoViewController()
+            case .like:
+                return LikesViewController()
+            }
+        }
+        
+        var title: String {
+            switch self {
+            case .topic:
+                return "토픽"
+            case .random:
+                return "랜덤 사진"
+            case .search:
+                return "검색"
+            case .like:
+                return "찜"
+            }
+        }
+        
+        var image: UIImage? {
+            switch self {
+            case .topic:
+                Image.tabTrendInactive
+            case .random:
+                Image.tabRandomInactive
+            case .search:
+                Image.tabSearchInActive
+            case .like:
+                Image.tabLikeInactive
+            }
+        }
+        
+        var selectedImage: UIImage? {
+            switch self {
+            case .topic:
+                Image.tabTrend
+            case .random:
+                Image.tabRandom
+            case .search:
+                Image.tabSearch
+            case .like:
+                Image.tabLike
+            }
+        }
+    }
 }
